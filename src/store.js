97861aa5a5b5d374b957035102623f7cc43eb869
Ian Firdaus <ianfirdaus31@gmail.com>
2020-10-03 22:27:46 +0700

    Improved code structure

--- src/store.js
+++ src/store.js
@@ -8,16 +8,30 @@ export default new Vuex.Store({
     barColor: 'rgba(0, 0, 0, .8), rgba(0, 0, 0, .8)',
     barImage: 'https://demos.creative-tim.com/material-dashboard/assets/img/sidebar-1.jpg',
     drawer: null,
+    access_token: localStorage.getItem('access_token'),
   },
+
   mutations: {
     SET_BAR_IMAGE (state, payload) {
       state.barImage = payload
     },
+
     SET_DRAWER (state, payload) {
       state.drawer = payload
     },
+
+    SET_ACCESS_TOKEN(state, payload) {
+      state.access_token = payload
+    },
   },
+
   actions: {
 
+  },
+
+  getters: {
+    isAuth: state => {
+      return state.access_token !== "null" && state.access_token != null
+    }
   },
 })
